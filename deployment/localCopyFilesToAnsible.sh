e6854723c8984b42c469d449b5bb698aa456d360
Unknown <mbdavid297@gmail.com>
2018-10-25 18:34:05 +0200

    Script to update IPs

--- deployment/localCopyFilesToAnsible.sh
+++
@@ -1,2 +0,0 @@
-read -p "Ansible Master Floating IP: " floatingAM
-scp QTLaaSRequiredFiles/ ubuntu@$floatingAM:/home/ubuntu/
---
+++ src/changeHostIPs.sh
@@ -0,0 +1,69 @@
+# Manually get the floating IPs
+
+# read -p "Ansible Master Floating IP: " floatingAM
+# read -p "Spark Master Floating IP: " floatingSM
+# read -p "Spark Worker Floating IP: " floatingSW
+
+###########################
+#echo "password" | source openrc.sh
+
+# Store the list of openstack IPs in a file
+openstack server list > serverlist
+
+# Filter our VMs and get their IPs
+
+full=$(grep 'ACC20-A-important' -r serverlist)
+ips=$(cut -d "=" -f 2 <<< $full)
+privA=${ips:0:12}
+floatingA=${ips:14:14}
+
+full=$(grep 'ACC20-SM-important' -r serverlist)
+ips=$(cut -d "=" -f 2 <<< $full)
+privSM=${ips:0:12}
+floatingSM=${ips:14:14}
+
+full=$(grep 'ACC20-S-important' -r serverlist)
+ips=$(cut -d "=" -f 2 <<< $full)
+privSW=${ips:0:12}
+floatingSW=${ips:14:14}
+
+# Remove unnecessary files
+rm serverlist
+
+# /etc/hosts For all nodes
+
+echo "127.0.0.1 localhost
+
+$privA ansible-node
+$privSW sparkworker1
+$privSM sparkmaster
+
+# The following lines are desirable for IPv6 capable hosts
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts" > exampleHostFile
+
+#mv exampleHostFile /etc/hosts
+#this should overwrite the host file, just paste it there for now 
+#scp exampleHostFile ubuntu@$floatingSM:/home/ubuntu/ 
+#scp exampleHostFile ubuntu@$floatingSW:/home/ubuntu/
+
+# /etc/ansible/hosts for ansible node
+
+echo "ansible-node ansible_ssh_host=$privA
+sparkmaster  ansible_ssh_host=$privSM
+sparkworker1 ansible_ssh_host=$privSW
+
+[configNode]
+ansible-node ansible_connection=local ansible_user=ubuntu
+
+[sparkmaster]
+sparkmaster ansible_connection=ssh ansible_user=ubuntu
+
+[sparkworker]
+sparkworker1 ansible_connection=ssh ansible_user=ubuntu" > exampleAnsibleHostsFile
+
+#mv exampleHostFile /etc/hosts
