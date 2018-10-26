67d590c60c6d5d8c1a72c9c2d8c422873adcb90c
MKarpe <maxkarpefors@student-229-172.eduroam.uu.se>
2018-10-26 15:01:57 +0200

    test a new idea

--- src/app.py
+++ src/app.py
@@ -10,28 +10,28 @@ app = Flask(__name__)
 def home():
         return render_template("home.html")
 
-@app.route('/create', methods=['POST'])
+@app.route('/create//<mess>', methods=['POST'])
 def create():
         amount = request.form['amount-workers']
-        user_mess = "Starting your cluster with " + amount + " workers..."
-        print user_mess
+        mess = "Starting your cluster with " + amount + " workers..."
+        print mess
         res = createspark.delay(True,amount)
         result=res.get()
-        render_template("home.html", message=user_mess)
+        render_template("home.html", message=mess)
         return jsonify(result)
 
 @app.route('/resize', methods=['POST'])
 def resize():
 		amount = request.form['new-amount-workers']
-		user_mess = "Resizing your cluster with" + amount + " workers..."
-		print user_mess
-		render_template("home.html", message=user_mess)
-        return render_template("home.html", message=user_mess)
+		mess = "Resizing your cluster with" + amount + " workers..."
+		print mess
+		render_template("home.html", message=mess)
+        return render_template("home.html", message = mess)
 
 @app.route('/remove', methods=['POST'])
 def remove():
-		user_mess = "Removes your cluster..."
-		print user_mess
+		mess = "Removes your cluster..."
+		print mess
         return render_template("home.html")
 
 
